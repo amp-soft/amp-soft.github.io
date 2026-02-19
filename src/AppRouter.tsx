bc3dee83d0a09f2f33e0ef2b966d0b9db64caf5c
aaron-ampsoft <aaron-ampsoft@gmail.com>
2026-02-19 16:53:44 -0500

    Ensuring route scrolls to top

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,24 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from '@/features/Home/HomePage';
 import NotFoundPage from '@/features/NotFound/NotFoundPage';
 import AuraDreamPrivacyPage from '@/features/Privacy/AuraDreamPrivacyPage';
 import RemindersPrivacyPage from '@/features/Privacy/RemindersPrivacyPage';
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+};
+
 export const AppRouter = () => {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/privacy/auradream" element={<AuraDreamPrivacyPage />} />
